01b7a9a782081c02816fb2c1136ec20edcf25a7b
Debora Rodrigues <deboral.p@hotmail.com>
2022-05-10 19:56:03 -0300

    Refatorar codigo e staoryboard

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -9,17 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var pesoTextField: UITextField!
-    @IBOutlet weak var alturaTextField: UITextField!
-    @IBOutlet weak var indiceLabel: UIView!
-    @IBOutlet weak var imagemView: UIImageView!
-    @IBAction func calcularButton(_ sender: Any) {
-    }
+    //MARK: - Outlets
+    @IBOutlet weak var tfWeigth: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var viResult: UIView!
+    var imc: Double = 0
+    //MARK: - ciclo de vida da view
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    //MARK: - Actions
+    @IBAction func calculate(_ sender: Any) {
+        if let weight = Double (tfWeigth.text!), let height = Double(tfHeight.text!){
+            imc = weight / (height*height)  //ou pow(height,2)
+            
+            showResults()
+        }
+    }
+    func showResults(){
+        var result: String = ""
+        var image: String = ""
+        switch imc{
+            case 0..<16:
+                result = "Magreza"
+                image = "abaixo"
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobre"
+        
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        lbResult.text = result
+        ivResult.image = UIImage (named: image)
+        viResult.isHidden = false
+    }
 }
 
