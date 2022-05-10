ceae30935970ca67553c4c0fdaff39779e0bfca7
Debora Rodrigues <deboral.p@hotmail.com>
2022-05-09 21:38:38 -0300

    Criar action e outlets na ViewController

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pesoTextField: UITextField!
+    @IBOutlet weak var alturaTextField: UITextField!
+    @IBOutlet weak var indiceLabel: UIView!
+    @IBOutlet weak var imagemView: UIImageView!
+    @IBAction func calcularButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
